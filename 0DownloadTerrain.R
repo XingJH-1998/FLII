cff4d7ef5a24b2d2188207fa9952a0a7dd0bd487
XingJH-1998 <tangerine0w0@163.com>
2025-07-09 20:55:21 +0800

    0709

---
+++ 0DownloadTerrain.R
@@ -0,0 +1,26 @@
+#Download Elevation Slope Terrain Ruggedness Index from Amatulli et al.
+library(data.table)
+library(tidyr)
+
+data <- read.table("/Users/tangerine/Downloads/GMTED_list-of-links-to-files.tab", 
+                   header = TRUE,    # 第一行为列名
+                   sep = "\t",       # 指定制表符分隔
+                   stringsAsFactors = FALSE)  # 字符列不转为因子
+
+data2 <- data %>%
+  separate(`File.name`, into = c("Source", "Type", "Rest"), sep = "\\|") %>%
+  mutate(across(c(Source, Type, Rest), trimws)) %>%
+  separate(Rest, into = c("Name", "LengthRes", "Type2"), sep = "_", remove = TRUE) %>%
+  extract(LengthRes, into = c("Length", "Res"), regex = "([0-9]+[A-Z]+)([a-zA-Z]+)$") %>%
+  filter(Type %in% c("elevation","slope","tri")) %>%
+  filter(Length == "1KM")
+urls <- data2$URL.file
+dest_files <- file.path("data", basename(urls))
+options(timeout = 3600)
+for(i in 14:18) {
+  download.file(urls[i], destfile = dest_files[i], mode = "wb")
+  Sys.sleep(60)
+}
+
+aet <- rast("/Users/tangerine/Downloads/TerraClimate_aet_2024.nc")
+
--- 0gittest.R
+++ 0gittest.R
@@ -9,6 +9,7 @@ flii <- rast("/Users/tangerine/Downloads/flii_earth_1k/flii_earth_1k.tif")
 
 # How many data quality information to keep?
 dd <- readRDS('/Volumes/Extreme SSD/GBIF202504/Aves/Anseriformes/Anatidae/Anas/Anas acuta/Anas acuta.rda')
+aves <- readRDS('/Volumes/Extreme SSD/GBIF202504/Aves.rda')
 colnames(dd)
 table(dd$taxonRank)
 table(dd$occurrenceStatus)
@@ -18,5 +19,10 @@ table(dd$basisOfRecord)
 summary(dd$elevation)
 
 # Time unit of env data: Year/Month/Average?
+
+ts_elv <- rast("/Users/tangerine/Downloads/elevation_1KMmn_GMTEDmn.tif")
+plot(ts_elv)
+ts_elv2 <- rast("/Users/tangerine/Downloads/elevation_1KMmn_SRTM.tif")
+plot(ts_elv2)
 
 
